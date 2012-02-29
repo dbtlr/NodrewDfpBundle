321bb5e1e03ee7aa81211af645796ecb24972f23
Drew Butler <drew@abstracting.me>
2012-02-29 16:44:48 -0500

    Added twig to the list of dependencies.

--- vendor/vendors.php
+++ vendor/vendors.php
@@ -10,6 +10,7 @@ if (isset($argv[1])) {
 $vendorDir = __DIR__;
 $deps = array(
     array('symfony', 'http://github.com/symfony/symfony', isset($_SERVER['SYMFONY_VERSION']) ? $_SERVER['SYMFONY_VERSION'] : 'origin/master'),
+    array('twig', 'http://github.com/fabpot/Twig', 'origin/master'),
 );
 
 foreach ($deps as $dep) {
