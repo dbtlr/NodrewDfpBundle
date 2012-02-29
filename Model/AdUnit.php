a57347dc42bb3e19494b1e61a9664928568a2c40
Drew Butler <drew@abstracting.me>
2012-02-29 12:38:28 -0500

    Added the AdUnit and Collection models.

---
+++ Model/AdUnit.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Nodrew\Bundle\DfpBundle\Model;
+
+/**
+ * @package     NodrewDfpBundle
+ * @author      Drew Butler <hi@nodrew.com>
+ * @copyright	(c) 2012 Drew Butler
+ * @license     http://www.opensource.org/licenses/mit-license.php
+ */
+class AdUnit
+{
+    
+}
